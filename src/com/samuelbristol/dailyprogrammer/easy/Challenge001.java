2acb3116852ebe25892c6dc0b5f0fca1fbde7f8f
Samuel Bristol <samuelbristol@gmail.com>
2015-12-28 14:43:51 -0600

    Add Challenge information

--- src/com/samuelbristol/dailyprogrammer/easy/Challenge001.java
+++ src/com/samuelbristol/dailyprogrammer/easy/Challenge001.java
@@ -2,15 +2,19 @@ package com.samuelbristol.dailyprogrammer.easy;
 
 /**
  * @author samuelbristol
- *
- */
+ * Challenge 1 - Easy - Reddit Daily Programmer
+ * Found at: https://www.reddit.com/r/dailyprogrammer/comments/pih8x/easy_challenge_1/
+ * Submitted by Reddit user: nottoobadguy
+ * Challenge: create a program that will ask the users name, age, and reddit username. 
+ * 		Have it tell them the information back, in the format:
+ * 		your name is (blank), you are (blank) years old, and your username is (blank).
+ * 		For extra credit, have the program log this information in a file to be 
+ * 		accessed later. */
+
 public class Challenge001 {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
