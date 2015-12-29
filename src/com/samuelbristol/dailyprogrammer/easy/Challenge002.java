3d355dca00f20e9328d57917d8553f00c6c9b095
Samuel Bristol <samuelbristol@gmail.com>
2015-12-29 15:23:57 -0600

    Add authoring and challenge comments

--- src/com/samuelbristol/dailyprogrammer/easy/Challenge002.java
+++ src/com/samuelbristol/dailyprogrammer/easy/Challenge002.java
@@ -1,5 +1,21 @@
 package com.samuelbristol.dailyprogrammer.easy;
 
+/**
+ * @author samuelbristol
+ * Challenge 2 - Easy - Reddit Daily Programmer
+ * Found at: https://www.reddit.com/r/dailyprogrammer/comments/pjbj8/easy_challenge_2/
+ * Submitted by Reddit user: nottoobadguy
+ * Challenge: Hello, coders! An important part of programming is being able to apply 
+ * 		your programs, so your challenge for today is to create a calculator application 
+ * 		that has use in your life. It might be an interest calculator, or it might be 
+ * 		something that you can use in the classroom. For example, if you were in physics 
+ * 		class, you might want to make a F = M * A calc.
+ * 
+ * EXTRA CREDIT: make the calculator have multiple functions! Not only should it be able to 
+ * 		calculate F = M * A, but also A = F/M, and M = F/A! 
+ * 
+ * */
+
 public class Challenge002 {
 
 	public static void main(String[] args) {
