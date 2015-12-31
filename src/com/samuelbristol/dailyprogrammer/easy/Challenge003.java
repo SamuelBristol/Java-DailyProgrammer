671255d40a076c21601645df62726a62c92e7fa0
Samuel Bristol <samuel.d.bristol@gmail.com>
2015-12-31 15:47:10 -0600

    Add Challenge003 class

---
+++ src/com/samuelbristol/dailyprogrammer/easy/Challenge003.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.samuelbristol.dailyprogrammer.easy;
+
+/**
+ * @author samuelbristol
+ * Challenge 3 - Easy - Reddit Daily Programmer
+ * Found at: https://www.reddit.com/r/dailyprogrammer/comments/pkw2m/2112012_challenge_3_easy/
+ * Submitted by Reddit user: nottoobadguy
+ * Challenge: Welcome to cipher day! 
+ * 		Write a program that can encrypt texts with an alphabetical caesar cipher. This cipher 
+ * 		can ignore numbers, symbols, and whitespace.
+ * 
+ * EXTRA CREDIT: add a "decrypt" function to your program!
+ * 
+ * */
+public class Challenge003 {
+	public static void main(String[] args) {
+		
+	}
+}
