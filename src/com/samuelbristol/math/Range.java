002f05988d5a132ee151840b82ab1ef07e76989c
Samuel Bristol <samuelbristol@gmail.com>
2015-12-30 21:39:44 -0600

    Add com.samuelbristol.math package, Add Range class, move intInRangeExclusive behavior to Range class

---
+++ src/com/samuelbristol/math/Range.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.samuelbristol.math;
+
+/**
+ * @author samuelbristol
+ * 
+ * Description: A class to encapsulate Range-related behavior
+ */
+public final class Range {
+	
+	private Range () {};
+	
+	public static boolean intInRangeExclusive(int target, int first, int second) {
+		int lower = Math.min(first, second);
+		int higher = Math.max(first, second);
+		boolean value;
+		
+		if (target > lower && target < higher) {
+			value = true;
+		} else {
+			value = false;
+		}
+		
+		return value;
+	}
+}
