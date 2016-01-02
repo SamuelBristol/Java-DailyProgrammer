1f8c204b31dfa352016baee40f2e7efc8f3e3936
Samuel Bristol <samuel.d.bristol@gmail.com>
2016-01-01 23:30:34 -0600

    Add cipher solution. Uses all printable ASCII characters except DEL

--- src/com/samuelbristol/dailyprogrammer/easy/Challenge003.java
+++ src/com/samuelbristol/dailyprogrammer/easy/Challenge003.java
@@ -17,6 +17,52 @@ package com.samuelbristol.dailyprogrammer.easy;
  * */
 public class Challenge003 {
 	public static void main(String[] args) {
+		// Get the common available characters to work with
+		char chars[] = getPrintableAsciiCharacters();
 		
+		char message[] = "Some say the world will end in fire".toCharArray();
+		System.out.println("Message: " + new String(message));
+		
+		// Encode the message with shift 3
+		char encodedMessage[] = encode(message, chars, 3);
+		System.out.println("Encoded message: " + new String(encode(message, chars, 3)));
+		System.out.println("Decoded message: " + new String(decode(encodedMessage, chars, 3)));
+	}
+	
+	public static char[] getPrintableAsciiCharacters() {
+		int firstChar = 32; // space character
+		int lastChar = 126; // ~ character
+		
+		// Properly size the char array
+		char chars[] = new char[lastChar - firstChar + 1];
+		for (int i = 0; i < chars.length; i++) {
+			// Fill the char array with the appropriate characters
+			// Cast the integer to char for simplicity
+			chars[i] = (char) (i + firstChar);
+		}
+		
+		return chars;
+	}
+	
+	public static char[] encode (char[] message, char[] chars, int shift) {
+		// encode the message with the shift amount
+		String encodedMessage = "";
+		for (int i = 0; i < message.length; i++) {
+			int newValue = ((int) message[i]) + shift % (chars.length - 1);
+			encodedMessage += (char) newValue;
+		}
+		
+		return encodedMessage.toCharArray();
+	}
+	
+	public static char[] decode (char[] message, char[] chars, int shift) {
+		// encode the message with the shift amount
+		String decodedMessage = "";
+		for (int i = 0; i < message.length; i++) {
+			int newValue = ((int) message[i]) - shift % (chars.length - 1);
+			decodedMessage += (char) newValue;
+		}
+		
+		return decodedMessage.toCharArray();
 	}
 }
