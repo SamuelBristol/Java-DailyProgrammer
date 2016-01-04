790110a8fc69286d6ba0afad27f06fadcf90e0c8
Samuel Bristol <samuel.d.bristol@gmail.com>
2016-01-03 18:13:48 -0600

    Add Challenge004 class

---
+++ src/com/samuelbristol/dailyprogrammer/easy/Challenge004.java
@@ -0,0 +1,19 @@
+package com.samuelbristol.dailyprogrammer.easy;
+
+/**
+ * @author samuelbristol
+ * Challenge 4 - Easy - Reddit Daily Programmer
+ * Found at: https://www.reddit.com/r/dailyprogrammer/comments/pm6oj/2122012_challenge_4_easy/
+ * Submitted by Reddit user: nottoobadguy
+ * Challenge: You're challenge for today is to create a random password generator!
+ * Extra Credit: Allow the user to specify the amount of passwords to generate. 
+ * 		Allow the user to specify the length of the strings he wants to generate!
+ * 
+ * */
+public class Challenge004 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
